e994dddf49c3b109efe1bb097e92bc65dbe2c905
sj3603 <56056374+sj3603@users.noreply.github.com>
2023-03-07 12:49:09 -0500

    Snail adjustments
    - Snail interactions tweaked
    - Size adjusted to be a bit smaller and more snail appropriate
    - Snails now appear once the player has become small enough

--- Project Mindful/Assets/Scripts/SizeVisibilityManager.cs
+++ Project Mindful/Assets/Scripts/SizeVisibilityManager.cs
@@ -10,13 +10,17 @@ public class SizeVisibilityManager : MonoBehaviour
     //The scale value the player needs to get under to activate small objects
     [SerializeField] float playerSizeThresholdSmall;
     //The game objects that become active once the player has reached the small size threshold
-    [SerializeField] GameObject smallObjectGroup;
+    [SerializeField] List<GameObject> smallObjectGroup;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        smallObjectGroup.SetActive(false);
+        foreach(GameObject obj in smallObjectGroup)
+        {
+            obj.SetActive(false);
+        }
+        
     }
 
     // Update is called once per frame
@@ -24,7 +28,10 @@ public class SizeVisibilityManager : MonoBehaviour
     {
         if(thirdPerson.transform.Find("Player").localScale.x <= playerSizeThresholdSmall || firstPerson.transform.Find("Player").localScale.x <= playerSizeThresholdSmall)
         {
-            smallObjectGroup.SetActive(true);
+            foreach (GameObject obj in smallObjectGroup)
+            {
+                obj.SetActive(true);
+            }
         }
     }
 }
